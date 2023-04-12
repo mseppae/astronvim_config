c77a2f2a5984aaf30619a9090145e49fefc81d2e
Marko Seppä <marko.seppa@gmail.com>
2023-04-12 21:24:15 +0200

    Support bundle exec etc

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,15 +2,45 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+    if not null_ls_status_ok then return config end
+
+    local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
+    local conditional = function(fn)
+      local utils = require("null-ls.utils").make_conditional_utils()
+      return fn(utils)
+    end
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
       -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier,
+
+      conditional(
+        function(utils)
+          return utils.root_has_file "Gemfile"
+              and null_ls.builtins.formatting.rubocop.with {
+                command = "bundle",
+                args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.formatting.rubocop._opts.args),
+              }
+            or null_ls.builtins.formatting.rubocop
+        end
+      ),
+
+      conditional(
+        function(utils)
+          return utils.root_has_file "Gemfile"
+              and null_ls.builtins.diagnostics.rubocop.with {
+                command = "bundle",
+                args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.diagnostics.rubocop._opts.args),
+              }
+            or null_ls.builtins.diagnostics.rubocop
+        end
+      ),
     }
     return config -- return final config table
   end,
