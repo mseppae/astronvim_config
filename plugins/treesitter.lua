d77ad1f4b2331aeada20a23298376dec4734d9cc
Marko Seppä <marko.seppa@gmail.com>
2023-11-16 20:54:26 +0100

    Cleanup treesitter configuration

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,26 +1,29 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = { "lua", "ruby", "javascript", "typescript", "go", "rust" },
-    incremental_selection = {
-      keymaps = {
-        init_selection = "<CR>",
-        node_decremental = "<S-TAB>",
-        node_incremental = "<TAB>",
-        scope_incremental = "<CR>",
+  opts = function(_, config)
+    local ensure_installed_extensions = { "lua", "ruby", "javascript", "typescript", "go" }
+    local config_replacements = {
+      incremental_selection = {
+        keymaps = {
+          init_selection = "<CR>",
+          node_decremental = "<S-TAB>",
+          node_incremental = "<TAB>",
+          scope_incremental = "<CR>",
+        },
       },
-    },
-  },
+      endwise = {
+        enable = true
+      }
+    }
+    -- Mutate the list by adding our preferences and keeping the defaults
+    vim.list_extend(config.ensure_installed, ensure_installed_extensions)
+    -- Merge our replacements to main config
+    local modified_config = vim.tbl_deep_extend("force", config, config_replacements)
+    return modified_config
+  end,
   dependencies = {
     {
       "RRethy/nvim-treesitter-endwise",
-      config = function()
-        require("nvim-treesitter.configs").setup {
-          endwise = {
-            enable = true,
-          },
-        }
-      end,
     },
   },
 }
