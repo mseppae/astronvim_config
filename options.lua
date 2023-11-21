dc8d8b20110d16effc103a3c3fb0b7d1d76003b6
Marko Seppä <marko.seppa@gmail.com>
2023-11-21 12:55:07 +0100

    Disable tabline as useless

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = true, -- sets vim.opt.wrap
     scrolloff = 999,
+    showtabline = 0, -- disable tabline
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
